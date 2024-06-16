448e13c5a6c4f2131c845b952aa6d2f875724a2d
Oleksandr <aleks.kapelian@gmail.com>
2024-06-16 16:59:19 +0200

    fixes

--- test/highload.js
+++ test/highload.js
@@ -10,13 +10,13 @@ async function exampleTransfer() {
   /* https://tonscan.org/address/EQD2Ub5nOuXcNoXHBhHCRgqn5ytOGS7NjxysRexEQd4sa794  */
 
   /* Wallet seed and address from exampleWallet, address is to check it is matched with transfers sender wallet */
-  const seed = 'c79f8c8a552c45b8ce97ee985cd7e58568186a79a9268dd57be9bae119830ea4'
-  const address = 'EQD2Ub5nOuXcNoXHBhHCRgqn5ytOGS7NjxysRexEQd4sa794'
+  const seed = ''
+  const address = ''
   /* Don't forget to deposit some TONs to the address ↑ */
 
   /* Put your test transfer here */
-  const recipient = 'EQDVmaZ4tfyEWXuIbZdJqmd-NVJdo7IUtj3vom-xWrlFWaNl'
-  const sendMessage = 'highload test'
+  const recipient = ''
+  const sendMessage = 'hello world'
   const tonAmount = 0.001
 
   /* Amount less than 0.001 TON will likely fail with code -14 */
@@ -26,7 +26,7 @@ async function exampleTransfer() {
   /* For provider using ton-lite-server */
 
   /* Example of multiple transactions in one blockchain request */
-  const transfersCount = 10
+  const transfersCount = 3
   const transfers = []
   const transfer = { recipient, sendMessage, tonAmount }
   for (let i = 0; i < transfersCount; i++) transfers.push(transfer)
@@ -34,5 +34,5 @@ async function exampleTransfer() {
   process.exit(0)
 }
 
-exampleWallet()
+// exampleWallet()
 // exampleTransfer()
